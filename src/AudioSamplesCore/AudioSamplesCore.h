7bf7aa00d74eb052919d5508369ba6a75c3d4228
Isaac Waldron <isaac@vardyn.com>
2014-12-10 18:21:23 -0500

    categorize functions in AudioSamplesCore.h

--- src/AudioSamplesCore/AudioSamplesCore.h
+++ src/AudioSamplesCore/AudioSamplesCore.h
@@ -11,6 +11,7 @@
 #define ASC_WAV24BIT_MAX  8388607
 #define ASC_WAV24BIT_MIN -8388607
 
+/* WAV format helpers */
 int asc_write_wav_sample_16bit(char *sample, int value);
 int asc_write_wav_sample_24bit(char *sample, int value);
 
