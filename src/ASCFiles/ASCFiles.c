39447cf277b86799c3da775a93e960b705422b09
Isaac Waldron <isaac@vardyn.com>
2015-01-12 22:54:59 -0500

    add logging output when clipping wav samples

--- src/ASCFiles/ASCFiles.c
+++ src/ASCFiles/ASCFiles.c
@@ -4,13 +4,20 @@
  */
 
 #include "ASCFiles.h"
+#include "ASCLogging.h"
 
 int asc_write_wav_sample_16bit(unsigned char *sample, int value)
 {
     if (ASC_WAV16BIT_MAX < value)
+    {
+        ASC_DEBUG("clipped 16-bit sample larger than maximum");
         value = ASC_WAV16BIT_MAX;
+    }
     else if (ASC_WAV16BIT_MIN > value)
+    {
+        ASC_DEBUG("clipped 16-bit sample smaller than minimum");
         value = ASC_WAV16BIT_MIN;
+    }
     
     sample[0] = value & 0xff;
     sample[1] = (value >> 8) & 0xff;
@@ -21,9 +28,15 @@ int asc_write_wav_sample_16bit(unsigned char *sample, int value)
 int asc_write_wav_sample_24bit(unsigned char *sample, int value)
 {
     if (ASC_WAV24BIT_MAX < value)
+    {
+        ASC_DEBUG("clipped 24-bit sample larger than maximum");
         value = ASC_WAV24BIT_MAX;
+    }
     else if (ASC_WAV24BIT_MIN > value)
+    {
+        ASC_DEBUG("clipped 24-bit sample smaller than minimum");
         value = ASC_WAV24BIT_MIN;
+    }
     
     sample[0] = value & 0xff;
     sample[1] = (value >> 8) & 0xff;
